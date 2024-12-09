8501fb90f02634e7f9eacaffc8eac95a507fd034
Pragati Gupta <pragatigupta0104@gmail.com>
2024-12-09 15:31:52 +0530

    aliasing

---
+++ calculator_for_python.py
@@ -0,0 +1,11 @@
+# ALIASING- IT refers as a if u want create nick name or short name of any file(module name)
+x=10
+y=20
+def add_calculator(x,y):
+#    return x+y
+     print(x+y)
+def sub(x,y):
+#   return x-y
+    print(x-y)
+def multiply_calculator(x,y):
+    print(x*y)
---
+++ new.py
@@ -0,0 +1,9 @@
+'''import calculator_for_python.py as my
+print (my.x)
+print(my.y)'''
+#from calculator_for_python import add_calculator as my
+#data=(my(5,10))
+#print(data)
+#my(20,10)
+from calculator_for_python import multiply_calculator as mul
+mul(4,5)
