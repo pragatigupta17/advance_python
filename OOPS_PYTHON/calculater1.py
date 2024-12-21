5c1770ed038d597f7dfff29aa2513d29928f2b42
Pragati Gupta <pragatigupta0104@gmail.com>
2024-12-21 15:17:20 +0530

    cal

---
+++ OOPS_PYTHON/calculater1.py
@@ -0,0 +1,21 @@
+class Calculater:
+    def add (*n):
+        sum=0
+        for i in n:
+            sum=sum+i
+        print(sum)
+    def sub(self,*n):
+        sub=0
+        for i in n:
+            sub=sub-i
+        print(sub)
+    def mul(*n):
+        mul=1
+        for i in n:
+            mul=mul*i
+        print(mul)
+    def div(*n):
+        div=1
+        for i in n:
+            div=div/i
+        print(div)
---
+++ OOPS_PYTHON/calculater2.py
@@ -0,0 +1,7 @@
+from calculater1 import Calculater as C
+#obj = Calculater()
+C.add(5, 6)  # Output: 11
+C.add(10,20,30)
+C.sub(10,20,30,40,50)
+C.mul(12,20)
+C.div(4,12)
